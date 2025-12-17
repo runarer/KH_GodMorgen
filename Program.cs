adbfd05f6c7abd9eca1caced094905d5a5bf3bb3
runarer <95697611+runarer@users.noreply.github.com>
2025-12-17 12:49:26 +0100

    Ask for name and print a basic greeting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Spør bruker om navn, inntil minst 3 bokstaver er skrevet inn
+string name = string.Empty;
+while(string.IsNullOrEmpty(name))
+{
+    Console.WriteLine("Skriv inn navet ditt");
+    name = Console.ReadLine() ?? "";
+}
+
+// Spør bruker om fødselsdato
+// Console.WriteLine("Skriv inn fødselsdato: YYYY-MM-DD");
+// DateTime birthDate;
+// string dateInput = string.Empty;
+// while(string.IsNullOrEmpty(dateInput))
+// {    
+//     dateInput = Console.ReadLine()  ?? "";
+//     bool success = DateTime.TryParse(dateInput,out birthDate);
+
+//     //  Hvis fødselsdato ikke er formatert riktig, si ifra og spør på nytt
+//     if(!success)
+//     {
+//         Console.WriteLine("Dato ble ikke skrevet inn riktig, bruk: YYYY-MM-DD");
+//         dateInput = "";
+//     }
+// }
+
+// Spør systemet om dagens dato og klokka
+DateTime today = new();
+
+// Få hilsen
+string greetings = Greetings(today);
+
+// Lag en dict med <ukedag,motiverende tekst>
+Dictionary<string,string> motivation = new()
+{
+    ["Monday"] = "kun fem dager igjen til helg",
+    ["Tuesday"] = "kun fire dager igjen til helg",
+    ["Wednesday"] = "kun tre dager igjen til helg",
+    ["Thursday"] = "kun to dager igjen til helg",
+    ["Friday"] = "bare idag igjen, så er det helg",
+    ["Saturday"] = "nå er det helg ",
+    ["Sunday"] = "siste dag av helgen",
+};
+
+// Skriv ut melding med <hilsen> <navn>, det er <ukedag> og klokka er <hh:mm>.
+Console.WriteLine($"{greetings} {name}, det er {(today.ToString("dddd"))}");
+
+// Skriv ut motiverende tekst.
+// Regn ut hvor mange dager brukeren har vært i live.
+// Skriv ut en melding med antall dager.
+
+// Lag hilsen basert på tidspunktet
+static string Greetings(DateTime dateTime)
+{
+    int hour = dateTime.Hour;
+
+    //  Hvis klokka er mellom 03-09 returner "God Morgen"
+    if(hour >= 3  && hour < 9)
+        return "God Morgen";
+
+    //  Hvis klokka er mellom 09-12 returner "God formiddag"
+    if(hour >= 9 && hour < 12)
+        return "God formiddag";
+
+    //  Hvis klokka er mellom 12-18 returner "God dag"
+    if(hour >= 12 && hour < 18)
+        return "God dag";
+
+    //  Hvis klokka er mellom 18-23 returner "God kveld"
+    if(hour >= 18 && hour < 23)
+        return "God kveld";
+    
+    //  returner "God natt"
+    return "God natt";
+}
+
+
+
+//Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
